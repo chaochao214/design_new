f3b7732b6db3c7d51db7ee2b1b0bb50e240dff74
505530098@qq.com <505530098@qq.com>
2020-06-28 20:54:03 +0800

    #resoure beanFactory 工厂方法创建

--- itheimaframeworkResource/src/main/java/com/itheima/Factory/XmlBeanFactory.java
+++ itheimaframeworkResource/src/main/java/com/itheima/Factory/XmlBeanFactory.java
@@ -1,8 +1,11 @@
 package com.itheima.Factory;
 
+import com.itheima.frameworkResource.util.ParseAnnotation;
 import com.itheima.frameworkResource.util.XmlBean;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.InputStream;
+import java.lang.reflect.Method;
 import java.util.Map;
 
 /**
@@ -14,14 +17,23 @@ public class XmlBeanFactory implements  BeanFactory{
     private  static Map<String,Object> beans;
 
 
-    public XmlBeanFactory() {
+    // 存储所有请求路径和对应的方法
+    private  static  Map<String, Method> methods;
+
+    //uri- id的映射关
+     private static  Map<String,String> urIIDmap;
+
+
+    public XmlBeanFactory() throws Exception {
         initBeans();
     }
 
     // 构造器initbean
-    private void initBeans() {
+    private void initBeans() throws Exception {
         beans = XmlBean.initBeans();
-
+        // 解析所有controller 里面有@Requestmapping 注解的对象， 并存档到map
+         methods= ParseAnnotation.parseRequestMapping();
+         urIIDmap= ParseAnnotation.parseUrlMappingInstance(methods,beans);
 
     }
 
@@ -40,6 +52,19 @@ public class XmlBeanFactory implements  BeanFactory{
 
     @Override
     public Object getUrlBean(String url) throws Exception {
+         String id= urIIDmap.get(url);
+         // 根据id获取位置值
+        if (StringUtils.isNotEmpty(id)) {
+             return getBean(id);
+        }
+
+
         return null;
     }
+
+    public static Map<String, Method> getMethods() {
+        return methods;
+    }
+
+
 }
